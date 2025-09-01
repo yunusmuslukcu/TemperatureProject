2d71019e68701d886397d51943904f3d30303dab
Yunus Muslukcu <ornek@email.com>
2025-09-01 18:05:49 +0300

    TemperatureProject

---
+++ script.js
@@ -0,0 +1,28 @@
+const celsius = document.getElementById("celcius");
+const fahrenheit = document.getElementById("fahrenheit");
+const kelvin = document.getElementById("kelvin");
+
+function calculateTemp(event) {
+  debugger;
+  const currentValue = Number(event.target.value);
+
+  switch (event.target.name) {
+    case "celcius":
+      kelvin.value = (currentValue + 273.32).toFixed(2);
+      fahrenheit.value = (currentValue * 1.8 + 32).toFixed(2);
+      break;
+
+    case "fahrenheit":
+      celsius.value = ((currentValue - 32) / 1.8).toFixed(2);
+      kelvin.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+      break;
+
+    case "kelvin":
+      celsius.value = (currentValue - 273.32).toFixed(2);
+      fahrenheit.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
+      break;
+
+    default:
+      break;
+  }
+}
